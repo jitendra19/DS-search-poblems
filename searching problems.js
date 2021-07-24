2bedd355e217925404abfb21238c127b6f5f00be
jitendra19 <jiten.cs2010@gmail.com>
2021-07-24 22:18:04 +0530

    1 file modified

--- searching problems.js
+++ searching problems.js
@@ -1,24 +1,18 @@
 export default function binarySearchIterative() {
-  console.log('abc');
   const input = [1, 4, 5, 6, 7, 8, 12, 30, 45, 78, 567];
-  const searchElement = 78;
+  const searchElement = 781; // 78
   let low = 0;
   let high = input.length - 1;
-  while (parseInt((high - low) / 2) > 0) {
-    console.log(input[parseInt((high - low) / 2)]);
-    if (input[parseInt((high - low) / 2)] === searchElement) {
-      console.log(parseInt((high - low) / 2));
-      return parseInt((high - low) / 2);
-    } else if (input[parseInt((high - low) / 2)] > searchElement) {
-      // console.log(parseInt((high - low) / 2));
-      high = parseInt((high - low) / 2);
-      break;
+  while (low !== high) {
+    if (input[parseInt((high + low) / 2)] === searchElement) {
+      // console.log(input[parseInt((high + low) / 2)]);
+      console.log(parseInt((high + low) / 2));
+      return parseInt((high + low) / 2);
+    } else if (input[parseInt((high + low) / 2)] > searchElement) {
+      high = parseInt((high + low) / 2);
     } else {
-      console.log(parseInt((high - low) / 2));
-      low = parseInt((high - low) / 2) + 1;
-      break;
+      low = parseInt((high + low) / 2) + 1;
     }
-    console.log(high);
-    high = 0;
   }
+  console.log(-1);
 }
